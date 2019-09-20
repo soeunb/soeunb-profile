743a9979a27cef38478992b6824f24e10d461e92
soeunb <b_soeun_@naver.com>
2019-09-20 23:10:25 +0900

    0920 ProfileContent : img v-for complete

--- src/components/ProfileContent.vue
+++ src/components/ProfileContent.vue
@@ -2,23 +2,24 @@
   <main>
     <div class="container">
       <div class="gallery">
-        <div v-for="(item, index) in items" :key="index" class="gallery-item">
-          <img :src="item.thumbnail" class="gallery-image"/>
-          <div class="gallery-item-info">
-            <ul>
-              <li class="gallery-item-comments"><span class="visually-hidden">Comments:</span><i class="fas fa-comment" aria-hidden="true"></i> {{ item.description }}</li>
-            </ul>
+        <div v-for="(item, index) in photos" :key="index" class="gallery-item">
+          <div @click="imgClick(item)" style="cursor:pointer;">
+            <img :src="item.thumbnail" class="gallery-image"/>
+            <div class="gallery-item-info">
+              <ul>
+                <li class="gallery-item-comments"><span class="visually-hidden">Comments:</span><i class="fas fa-comment" aria-hidden="true"></i> {{ item.description }}</li>
+              </ul>
+            </div>
           </div>
-          <button type="button" @click="imgClick(index)" :class="`img-index--${index}`">btton-{{ index }}</button>
+          <ProfileDetail v-if="item.show" @close="item.show = false">
+            <div slot="body">
+              <img :src="item.thumbnail" class="gallery-image-2" :class="`img-index--${index}`"/>
+            </div>
+            <h3 slot="body">body바꾸기</h3>
+            <h3 slot="footer">footer바꾸기</h3>
+          </ProfileDetail>
         </div>
       </div>
-      <ProfileDetail v-if="showModal" @close="showModal = false">
-        <div slot="body" v-for="(item, index) in items" :key="index" class="gallery-item">
-          <img :src="item.thumbnail" class="gallery-image-2" :class="`img-index--${index}`"/>
-        </div>
-        <h3 slot="body">body바꾸기</h3>
-        <h3 slot="footer">footer바꾸기</h3>
-      </ProfileDetail>
     </div>
   </main>
 </template>
@@ -32,12 +33,19 @@ export default {
   },
   data() {
     return {
-      showModal: false
+      //showModal: false,
+      photos: {}
     }
   },
+  created() {
+    this.photos = this.items.map(item => {
+      return { ...item, show: false }
+    })
+  },
   methods: {
-    imgClick(index) {
-      this.showModal = !this.showModal;
+    imgClick(item) {
+      //this.showModal = !this.showModal;
+      item.show = true
     }
   },
   components: {
